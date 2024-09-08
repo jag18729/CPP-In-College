a00aa5bd7362130152b8fd660ab33691542f81f9
Rafael J Garcia <ekoe299@gmail.com>
2024-09-07 17:52:37 -0700

    Adding Pointers

--- Pointers/main.cpp
+++ Pointers/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+using namespace std;
+
+void CD(int count) {
+    while (count > 0) {
+        cout<< count<< endl;
+        count = count -1;
+    }
+    cout << "Go!\n";
+}
+
+void countDown (int count) {
+    if (count <= 0) {
+        cout << "Go!\n";
+    } else {
+        cout<<count<<endl;
+        countDown(count - 1);
+        }
+    }
 
 int main()
 {
+    cout << "hello world!";    
     return 0;
 }
